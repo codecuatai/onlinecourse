cc5b80d56fcff0949c060bf5b3137af722c940a6
codecuatai <vtai2805@gmail.com>
2025-12-03 11:01:49 +0700

    start project and database

--- config/Database.php
+++ config/Database.php
@@ -1,1 +1,22 @@
-sdfghj
+<?php
+const _HOST = "localhost:3307";
+const _DB = "onlinecourse";
+const _USER = "root";
+const _PASS = "";
+const _DRIVER = "mysql";
+
+const _DEBUG = true;
+
+try {
+    if (class_exists("PDO")) {
+        $option = array(
+            PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        );
+        $dsn = _DRIVER . ':host=' . _HOST . ";dbname=" . _DB;
+        $conn = new PDO($dsn, _USER, _PASS, $option);
+        echo 'kết nối thành công';
+    }
+} catch (Exception $ex) {
+    echo 'Lỗi kết nối ' . $ex->getMessage();
+}
