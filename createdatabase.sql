7c6c5eb2f7b831ac9eddeb05d84e673fddc077d0
codecuatai <vtai2805@gmail.com>
2025-12-03 09:26:09 +0700

    start project

---
+++ createdatabase.sql
@@ -0,0 +1,81 @@
+﻿CREATE DATABASE onlinecourse;
+USE onlinecourse;
+
+-- bảng users
+CREATE TABLE users (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    fullname VARCHAR(255) NOT NULL,
+    role INT NOT NULL DEFAULT 0, 
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+-- bảng categories
+CREATE TABLE categories (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    description TEXT,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+-- bảng courses
+CREATE TABLE courses (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(255) NOT NULL,
+    description TEXT,
+    instructor_id INT NOT NULL,
+    category_id INT NOT NULL,
+    price DECIMAL(10,2) DEFAULT 0.00,
+    duration_weeks INT,
+    level VARCHAR(50),
+    image VARCHAR(255),
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (instructor_id) REFERENCES users(id),
+    FOREIGN KEY (category_id) REFERENCES categories(id)
+);
+
+-- bảng erollments
+CREATE TABLE enrollments (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    course_id INT NOT NULL,
+    student_id INT NOT NULL,
+    enrolled_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+    status VARCHAR(50) DEFAULT 'active', 
+    progress INT DEFAULT 0, 
+
+    FOREIGN KEY (course_id) REFERENCES courses(id),
+    FOREIGN KEY (student_id) REFERENCES users(id)
+);
+
+
+-- bảng lessons
+CREATE TABLE lessons (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    course_id INT NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    content LONGTEXT,
+    video_url VARCHAR(255),
+    lesson_order INT,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (course_id) REFERENCES courses(id)
+);
+
+
+-- bảng materials
+CREATE TABLE materials (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    lesson_id INT NOT NULL,
+    filename VARCHAR(255),
+    file_path VARCHAR(255),
+    file_type VARCHAR(50),
+    uploaded_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (lesson_id) REFERENCES lessons(id)
+);
+
+
